b74bd49a61455efb60cfb31f9e224efa3a8759be
HemantTheCodingDev <131195686+HemantTheCodingDev@users.noreply.github.com>
2023-05-02 08:20:38 -0400

    Add files via upload

---
+++ activity-feed-copy-link-functions.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * BuddyPress Activity Feed Copy Link
+ *
+ * Plugin Name:       Activity Feed Copy Link For BuddyPress
+ * Plugin URI:        #
+ * Description:       This plugin adds a "Copy Link" button to each activity feed in BuddyPress.
+ * Version:           1.0.0
+ * Author:            Hemant Jha
+ * Author URI:        https://www.linkedin.com/in/hemant-jha-3920b3120
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       buddypress-activity-feed-copy-link
+ * Domain Path:       /languages
+ *
+ * @package           Buddypress-activity-feed-copy-link
+ * @link              #
+ * @since             1.0.0
+ */
+
+define( 'BP_ACTIVITY_FEED_COPY_LINK_URL', plugin_dir_url( __FILE__ ) );
+define( 'BP_ACTIVITY_FEED_COPY_LINK_PATH', plugin_dir_path( __FILE__ ) );
+
+// Enqueue FontAwesome for the icon
+add_action('wp_enqueue_scripts', 'enqueue_fontawesome');
+function enqueue_fontawesome() {
+    wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
+}
+
+/** Bp_activity_feed_copy_link_enqueue_scripts */
+function bp_activity_feed_copy_link_enqueue_scripts() {
+	wp_enqueue_style( 'bp-activity-feed-copy-link-css', BP_ACTIVITY_LINK_PREVIEW_URL . 'assets/css/bp-activity-feed-copy-link.css', array(), '1.0.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'bp_activity_feed_copy_link_enqueue_scripts' );
+
+// Add copy functionality to each activity feed
+function bp_share_activity_filter_new() { ?>
+    <div class="generic-button">
+		<?php $activity_link_copy = bp_get_activity_thread_permalink(); ?>
+		<i class="far fa-copy" style="cursor: pointer;" onClick='copyText(this)'><span style="position: absolute;left: -9999px;"><?php echo $activity_link_copy; ?></span><span class="copy_activity_link"> Copy Link</span></i>
+	</div>
+	<script>
+		function copyText(element) {
+			var range, selection, worked;
+
+			if (document.body.createTextRange) {
+				range = document.body.createTextRange();
+				range.moveToElementText(element);
+				range.select();
+			} else if (window.getSelection) {
+				selection = window.getSelection();        
+				range = document.createRange();
+				range.selectNodeContents(element);
+				selection.removeAllRanges();
+				selection.addRange(range);
+			}
+
+			try {
+				document.execCommand('copy');
+				alert('Link copied');
+			}
+			catch (err) {
+				alert('unable to copy link');
+			}
+		}
+	</script>
+	<?php
+}
+add_action( 'bp_activity_entry_meta', 'bp_share_activity_filter_new' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Activity Feed Copy Link For BuddyPress ===
+Contributors: Hemant Jha
+Tags: buddypress, activity feed, copy link
+Requires at least: 4.0
+Tested up to: 5.8
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds a "Copy Link" button to each activity feed in BuddyPress.
+
+== Description ==
+
+Activity Feed Copy Link For BuddyPress adds a "Copy Link" button to each activity feed in BuddyPress. This allows users to easily copy the link to a specific activity and share it with others.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/activity-feed-copy-link-for-buddypress` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+
+== Frequently Asked Questions ==
+
+= Does this plugin require BuddyPress to be installed? =
+
+Yes, this plugin requires BuddyPress to be installed and activated on your WordPress site.
+
+== Screenshots ==
+
+1. The "Copy Link" button added to an activity feed in BuddyPress.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
